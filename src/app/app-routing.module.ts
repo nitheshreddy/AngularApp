06fc4319b3697beb1f9a4747fb570962f44bf5c5
nkethireddy <nitheshkethireddy@rockcentraldetroit.com>
2022-04-04 22:59:09 -0400

    Adding default Route to Routingmodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 
+const routes: Routes = [
+  { path: '**', redirectTo: '' } //Default route - Should be last entry in the routes
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
